d3329af5dfa0e7dc495a72a2782f9d6cce597401
Israel Meiresonne <53298@etu.he2b.be>
2020-05-14 19:52:47 +0200

    add logo esi in 'home' page

--- project/view/template.php
+++ project/view/template.php
@@ -2,10 +2,12 @@
 <html>
 <title><?= $title ?></title>
 <meta charset="UTF-8">
-<base href="<?= $webRoot ?>" >
+<base href="<?= $webRoot ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet" href="content/css/w3_home.css">
+<link rel="stylesheet" href="content/css/element.css">
+<link rel="stylesheet" href="content/css/home.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
 <style>
 
@@ -14,16 +16,35 @@
 <body>
 
     <div class="bgimg w3-display-container w3-animate-opacity w3-text-white">
-        <div class="w3-display-topleft w3-padding-large w3-xlarge">
-            Logo
+        <div class="esi-logo-container">
+            <div class="img-text-wrap">
+                <div class="img-text-img">
+                    <img src="content/images/static/logo-esi.png">
+                </div>
+                <div class="img-text-div">
+                    <div class="data-key_value-wrap">
+                        <span class="data-key_value-key">matricule:</span>
+                        <span class="data-key_value-value">53298</span>
+                    </div>
+                    <div class="data-key_value-wrap">
+                        <span class="data-key_value-key">nom:</span>
+                        <span class="data-key_value-value">meiresonne</span>
+                    </div>
+                    <div class="data-key_value-wrap">
+                        <span class="data-key_value-key">prénom:</span>
+                        <span class="data-key_value-value">israel</span>
+                    </div>
+                </div>
+            </div>
         </div>
+        <!-- <img src="content/images/static/logo-esi.png" style="width: 50px;"> -->
+        <!-- <div class="w3-display-topleft w3-padding-large w3-xlarge">
+            logo
+        </div> -->
         <div class="w3-display-middle">
             <h1 class="w3-jumbo w3-animate-top">COMING SOON</h1>
             <hr class="w3-border-grey" style="margin:auto;width:40%">
             <p class="w3-large w3-center">35 days left</p>
-        </div>
-        <div class="w3-display-bottomleft w3-padding-large">
-            Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a>
         </div>
     </div>
 
